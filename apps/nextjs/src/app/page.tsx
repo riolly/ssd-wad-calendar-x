25436df8624dcbc516801d2b2d736cbbb3c7fa48
riolly <alberto.riolly@gmail.com>
2023-07-27 16:53:07 +0700

    wip: calendar layout

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,52 +1,102 @@
-import { Suspense } from "react";
-import Link from "next/link";
-import { currentUser, SignOutButton } from "@clerk/nextjs";
+function getMonthString(date: Date) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return months[date.getMonth()];
+}
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-import { CreatePostForm, PostList } from "./posts";
+function getNumberOfDays(date: Date) {
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  return days[date.getMonth()];
+}
 
 export default function HomePage() {
-  return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
-        </h1>
-        <AuthShowcase />
+  const todayDate = new Date();
+  const year = todayDate.getFullYear();
+  const monthStr = getMonthString(todayDate);
 
-        <CreatePostForm />
-        <Suspense fallback={<span>Loading...</span>}>
-          <PostList />
-        </Suspense>
+  const firstDate = new Date(todayDate.setDate(1));
+
+  const daysPrev = Array(firstDate.getDay()).fill({
+    day: 0,
+    active: false,
+  }) as DayProps[];
+  const daysCurr = Array.from(
+    { length: getNumberOfDays(todayDate)! },
+    (_, i) => ({ day: i + 1, active: todayDate.getDay() === i + 1 }),
+  );
+  let daysAftr: DayProps[] = [];
+  if ((daysPrev.length + daysCurr.length) % 7 !== 0) {
+    const t = Math.ceil((daysPrev.length + daysCurr.length) / 7);
+    daysAftr = Array(t * 7 - (daysPrev.length + daysCurr.length)).fill({
+      day: 0,
+      active: false,
+    }) as DayProps[];
+  }
+  const daysObj = [...daysPrev, ...daysCurr, ...daysAftr];
+
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-blue-950 to-slate-900 text-white">
+      <div className="container flex flex-col items-center py-12">
+        <h1 className="mb-8 text-4xl">
+          {monthStr} {year}
+        </h1>
+
+        <div className="grid grid-cols-7 overflow-clip rounded-t-xl">
+          {days.map((day) => (
+            <DayHeading key={day} day={day} />
+          ))}
+          {daysObj.map((item, i) => (
+            <DayCard key={`${i}-${item.day}`} {...item} />
+          ))}
+        </div>
       </div>
     </main>
   );
 }
 
-async function AuthShowcase() {
-  const user = await currentUser();
+function DayHeading(props: { day: string }) {
+  return (
+    <div className="flex justify-center bg-blue-100/75 py-2 text-lg font-semibold text-slate-800">
+      {props.day}
+    </div>
+  );
+}
 
-  if (!user) {
-    return (
-      <Link
-        href="/sign-in"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-      >
-        Sign in
-      </Link>
-    );
-  }
+interface DayProps {
+  day: number;
+  active: boolean;
+}
+function DayCard({ day, active }: DayProps) {
+  const outsideDay = day === 0;
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        <span>Logged in as {user.firstName}</span>
-      </p>
-
-      <SignOutButton>
-        <button className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
-          Sign out
-        </button>
-      </SignOutButton>
+    <div
+      className="h-32 w-44 border border-white border-opacity-30 bg-slate-900 bg-opacity-30 p-2 text-zinc-100 text-opacity-60 data-[active=true]:border-opacity-100 data-[outside=true]:bg-gray-500/50 data-[active=true]:text-xl data-[active=true]:text-opacity-100"
+      data-outside={outsideDay}
+      data-active={active}
+    >
+      {!outsideDay && day}
     </div>
   );
 }
