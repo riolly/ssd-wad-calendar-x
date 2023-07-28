757dfdd0dfe474855f862aa7d9fece4e8dedecd6
riolly <alberto.riolly@gmail.com>
2023-07-28 14:43:25 +0700

    style: isToday underline

--- apps/nextjs/src/app/DateCell.tsx
+++ apps/nextjs/src/app/DateCell.tsx
@@ -41,7 +41,9 @@ export default function DayCell({
           "border-2 border-blue-300 border-opacity-100 font-bold text-opacity-100",
       )}
     >
+      <span className={cn(isToday && "underline underline-offset-4")}>
       {dated.date}
+      </span>
       <div
         className="absolute -left-0.5 -top-0.5 box-content flex h-full w-full flex-col items-center justify-center gap-3 rounded-xl border-2 border-blue-100 bg-gray-500/20 text-white opacity-0 backdrop-blur transition-opacity hover:opacity-100 group-focus:opacity-100 data-[selected=true]:bg-blue-500 data-[selected=true]:opacity-100"
         data-selected={isSelected}
