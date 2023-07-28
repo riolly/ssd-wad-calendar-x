b92a093f6063b33adf2d8f2facc5aaf0a6e59fff
riolly <alberto.riolly@gmail.com>
2023-07-28 15:09:06 +0700

    chore: validation note

--- apps/nextjs/src/lib/date.ts
+++ apps/nextjs/src/lib/date.ts
@@ -27,14 +27,18 @@ export const MONTHS = [
 ];
 
 export function getMonthString(date: Date) {
+  // fix: zod validation for number <12
   return MONTHS[date.getMonth()]!;
 }
 
 export function getDayString(date: Date) {
+  // fix: zod validation for number <=7
   return DAYS[date.getDay()]!;
 }
 
 export const getDateOrdinal = (date: Date | number): Ordinal => {
+  // fix: zod validation for number <=31
+
   const d = typeof date === "number" ? date : date.getDay();
   if (d > 3 && d < 21) return "th";
   switch (d % 10) {
@@ -50,6 +54,7 @@ export const getDateOrdinal = (date: Date | number): Ordinal => {
 };
 
 export function getNumberOfDays(dateOrMonth: Date | number) {
+  // fix: zod validation for number <12
   const daysLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   const idx =
